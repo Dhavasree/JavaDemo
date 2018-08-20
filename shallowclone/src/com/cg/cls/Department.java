0426a40f6f769e09643a695c1dc186f649aaba29
dsathiya <dsathiya@DIN13003237.corp.capgemini.com>
2018-08-20 13:30:04 +0530

    change the clone name

---
+++ shallowclone/src/com/cg/cls/Department.java
@@ -0,0 +1,14 @@
+package com.cg.cls;
+
+public class Department {
+	
+int dep1;
+int dep2;
+int dep3;
+public Department(int dep1, int dep2, int dep3) {
+	super();
+	this.dep1 = dep1;
+	this.dep2 = dep2;
+	this.dep3 = dep3;
+}
+}
